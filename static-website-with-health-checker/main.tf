f6732adce34b5142856b8b0aad7f6b65b4e54c58
PrakyathReddy <prakyathreddyk@gmail.com>
2025-09-30 08:44:44 +0530

    added tf file to define security group and assign to an ec2 instance

---
+++ static-website-with-health-checker/main.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "web_sg" {
+    name = "web-sg-demo"
+    description = "allow ssh and http"
+
+    ingress = {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # open to the world 
+    }
+
+    ingress {
+        description = "Allow all outbound"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Allow all outbound"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+
+    tags {
+        Name = "web-sg-demo"
+    }
+}
+
+resource "aws_instance" "web" {
+    ami = ""
+    instance_type = "t3.micro"
+    security_groups = [aws_security_group.web_sg.id]
+
+    tags {
+        Name: "TerraformDemoInstance"
+    }
+}
